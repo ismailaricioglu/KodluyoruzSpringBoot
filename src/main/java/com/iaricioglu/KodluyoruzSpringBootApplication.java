743f4c6e6ef662058a0255a47116de81b99f18c5
ismailaricioglu <ggl.issoline@gmail.com>
2023-12-12 21:11:24 +0300

    Düzenlendi.

--- src/main/java/com/iaricioglu/KodluyoruzSpringBootApplication.java
+++ src/main/java/com/iaricioglu/KodluyoruzSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KodluyoruzSpringBootApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KodluyoruzSpringBootApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(KodluyoruzSpringBootApplication.class, args);
+	}
 
 }
