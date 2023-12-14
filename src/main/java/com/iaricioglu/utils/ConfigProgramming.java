05afa666d6651efddd4682b8347a1786030aae6b
ismailaricioglu <ggl.issoline@gmail.com>
2023-12-14 18:35:48 +0300

    DevTools Aktif Etmek

---
+++ src/main/java/com/iaricioglu/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.iaricioglu.utils;
+
+public class ConfigProgramming {
+    // DevTools ==> LiveReload
+    // file ==> settings ==> compiler ==> Build project automatically (Ctrl+Alt+s)
+    // file ==> Advanced_Settings ==> Allow_Auto-make-to-start_even (OK clicked)
+}
