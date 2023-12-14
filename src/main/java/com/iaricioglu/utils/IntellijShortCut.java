8dd2a0b0edd17e43a2c48e87f8ff63190d5d1c7c
ismailaricioglu <ggl.issoline@gmail.com>
2023-12-14 18:34:56 +0300

    Kısa yollar

---
+++ src/main/java/com/iaricioglu/utils/IntellijShortCut.java
@@ -0,0 +1,8 @@
+package com.iaricioglu.utils;
+
+public class IntellijShortCut {
+//    Setting ==> ctrl+alt+s
+//    Alt+Ins ==> constructor, toString, getter, setter
+//    Ctrl+/ ==> single_omment
+//    Ctrl+shift+/ ==> multible_comment
+}
