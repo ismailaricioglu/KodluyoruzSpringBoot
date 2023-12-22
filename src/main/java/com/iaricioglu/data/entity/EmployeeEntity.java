8d8c0a7eeb85e7eb0a343b258ecb5a89418134cd
ismailaricioglu <ggl.issoline@gmail.com>
2023-12-22 20:25:48 +0300

    H2'nin çalışması için revize (Pom.xml, application.properties)

--- src/main/java/com/iaricioglu/data/entity/EmployeeEntity.java
+++ src/main/java/com/iaricioglu/data/entity/EmployeeEntity.java
@@ -3,6 +3,7 @@ package com.iaricioglu.data.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
