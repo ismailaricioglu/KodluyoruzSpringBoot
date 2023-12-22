a5e448f55473f61b92b48c6754e3bdefafa04dbf
ismailaricioglu <ggl.issoline@gmail.com>
2023-12-22 15:36:10 +0300

    Success all CRUD (Create-Read(find by id - list)-Update-Delete )

--- src/main/java/com/iaricioglu/data/repository/EmployeeRepository.java
+++ src/main/java/com/iaricioglu/data/repository/EmployeeRepository.java
@@ -1,10 +1,11 @@
 package com.iaricioglu.data.repository;
 
+import com.iaricioglu.data.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 //JpaRepository>CrudRepository kapsama ifadesi
 @Repository
-public interface EmployeeRepository extends CrudRepository {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
 }
---
+++ src/main/java/com/iaricioglu/utils/Postman.java
@@ -0,0 +1,38 @@
+package com.iaricioglu.utils;
+
+public class Postman {
+
+    //LIST
+    //GET ==> http://localhost:8080/api/v1/employees
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+    //FIND
+    //GET ==> http://localhost:8080/api/v1/employees/3
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    //DELETE
+    //DELETE ==> http://localhost:8080/api/v1/employees/3
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    //SAVE
+    //POST ==> http://localhost:8080/api/v1/employees
+    //body => raw ==>JSON
+    //    {
+    //        "firstName": "İsmail3",
+    //            "lastName": "Arıcıoğlu3",
+    //            "emailId": "ismailaricioglu3@gmail.com"
+    //    }
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+    //UPDATE
+    //PUT ==> http://localhost:8080/api/v1/employees/6
+    //body => raw ==>JSON
+    //    {
+    //        "firstName": "İsmail3",
+    //            "lastName": "Arıcıoğlu3",
+    //            "emailId": "ismailaricioglu3@gmail.com"
+    //    }
+    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+}
